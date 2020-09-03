e097f333f929a9930d5e939f742456280e66ff37
Maycie Morris <maycie.morris@gmail.com>
2020-09-03 13:28:48 -0400

    completed mvp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import useDarkMode from '../hooks/useDarkMode'
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
+    document.body.classList.toggle("dark-mode")
     setDarkMode(!darkMode);
   };
   return (
